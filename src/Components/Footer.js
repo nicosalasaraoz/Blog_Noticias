cc3b4c0bc022b4f212f18b5f8437c14694085884
unknown <busimaximiliano@gmail.com>
2022-11-15 23:39:29 -0300

    armando proyecto

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mt-auto bg-dark text-white">
+        <h5>Aqui va el footer</h5>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-dark text-white">
+      <h5>Aqui va el navbar..</h5>
+    </div>
+  );
+};
+
+export default Header;
