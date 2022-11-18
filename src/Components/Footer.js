1ed55fdc154876cf0708192617f0fded79954c88
CintiaUtrera <cintiaa_utrera@outlook.es>
2022-11-18 17:40:09 -0300

    Agregando iconos

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link} from 'react-router-dom';
-import { SlSocialFacebook } from "react-icons/sl";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -10,7 +10,7 @@ const Footer = () => {
         <div className="container">
           <nav className="row">
             <Link to='/' className="col-12 col-md-3 d-flex aling-items-center justify-content-center">
-              <img src="../images/logo.png" className="mx-2" height="60" alt="logo"/>
+              <img src="../images/logo.png" className="mx-2 my-4" height="60" alt="logo"/>
             </Link> 
             <ul className="col-12 col-md-3 list-unstyled">
               <li className="mb-2"><Link to='/error' className="fw-bold text-white" style={{textDecoration:"none"}}>Últimas noticias</Link></li>
@@ -23,10 +23,15 @@ const Footer = () => {
               <li className="mb-2"><Link to='/error' className="fw-bold text-white" style={{textDecoration:"none"}}>Términos & Condiciones</Link></li>
             </ul>
             <ul className="col-12 col-md-3 list-unstyled">
-              <li className="mb-2 fw-bold">Síguenos</li>
-              <li className="d-flex justify-content-between"><Link to='/error' className="text-primary">
-                <SlSocialFacebook /></Link></li>
-              
+              <li className="mb-2 fw-bold"></li>
+              <li className="d-flex justify-content-evenly">
+                <Link to='/error' className="icono text-center">
+                <FaFacebookF /></Link>
+                <Link to='/error' className="icono text-center">
+                <FaTwitter /></Link>
+                <Link to='/error' className="icono text-center">
+                <FaInstagram /></Link>
+                </li>
             </ul>
           </nav>
         </div>
