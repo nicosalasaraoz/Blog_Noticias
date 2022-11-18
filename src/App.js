703406ba2304429d358aceb56219cb5ba1824514
CintiaUtrera <cintiaa_utrera@outlook.es>
2022-11-17 21:57:37 -0300

    ubicando logo en el footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { Container } from "react-bootstrap";
 import "./css/globals.css";
 import Layout from "./pages/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Container fluid className="d-flex flex-column min-vh-100 px-0">
         <Layout />
       </Container>
+      </BrowserRouter>
     </>
   );
 }
