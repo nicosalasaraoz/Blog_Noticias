d446206f35519f012c7ace834b682ac8bf6b4648
unknown <busimaximiliano@gmail.com>
2022-11-18 21:54:00 -0300

    avanzando con navbar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,11 +2,7 @@ import React from "react";
 import Main from "../Components/Main";
 
 const Layout = () => {
-  return (
-    <div>
-      <Main />
-    </div>
-  );
+  return <Main />;
 };
 
 export default Layout;
