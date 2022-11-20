20ca8f6c53956b8ac667467a070964ca7cde7efb
CintiaUtrera <cintiaa_utrera@outlook.es>
2022-11-20 14:40:59 -0300

    creando footer en modo mobile

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,19 +5,19 @@ import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
     <>
-    <div>
-      <footer className="mt-auto bg-footer py-4 text-white">
+    <div className="mt-auto ">
+      <footer className=" bg-footer py-4 text-white pb-0">
         <div className="container">
           <nav className="row">
             <Link to='/' className="col-12 col-md-4 col-lg-3 d-flex aling-items-center justify-content-center">
               <img src="../images/logo.png" className="mx-2 my-4 m-4" height="70" alt="logo"/>
             </Link> 
-            <ul className="col-12 col-md-4 col-lg-3 list-unstyled text-center">
+            <ul className="col-12 col-md-4 col-lg-3 list-unstyled text-center oculto">
               <li className="mb-2"><Link to='/error' className="fw-bold text-white" style={{textDecoration:"none"}}>Últimas Noticias</Link></li>
               <li className="mb-2"><Link to='/error' className="fw-bold text-white" style={{textDecoration:"none"}}>Opinión</Link></li>
               <li className="mb-2"><Link to='/error' className="fw-bold text-white" style={{textDecoration:"none"}}>Edición Impresa</Link></li>
             </ul>
-            <ul className="col-12 col-md-4 col-lg-3 list-unstyled text-center">
+            <ul className="col-12 col-md-4 col-lg-3 list-unstyled text-center oculto">
               <li className="mb-2"><Link to='/error' className="fw-bold text-white" style={{textDecoration:"none"}}>Clasificados</Link></li>
               <li className="mb-2"><Link to='/error' className="fw-bold text-white" style={{textDecoration:"none"}}>Politicas de Privacidad</Link></li>
               <li className="mb-2"><Link to='/error' className="fw-bold text-white" style={{textDecoration:"none"}}>Términos & Condiciones</Link></li>
@@ -32,12 +32,17 @@ const Footer = () => {
                 <Link to='/error' className="icono-footer text-center">
                 <FaInstagram /></Link>
                 </li>
+                <li className="mt-3 fw-bold text-center ">
+                <Link to='/contacto'className="text-white" style={{textDecoration:"none"}}>Contáctenos</Link>
+                </li>
             </ul>
           </nav>
         </div>
       </footer>
-      </div>
-      
+      <div className="text-center bg-footer ">
+          <p className="text-white mb-0 ">&copy; BlogNews. Comision 9i-Grupo3. 2022</p>
+        </div>
+    </div>
     </>
   );
 };
