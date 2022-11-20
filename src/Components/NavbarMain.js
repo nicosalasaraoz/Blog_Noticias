cbc0da40d2080e7e716fd45cd35daddfe98e9950
unknown <busimaximiliano@gmail.com>
2022-11-20 12:57:54 -0300

    agregando botones navbar

--- src/Components/NavbarMain.js
+++ src/Components/NavbarMain.js
@@ -8,34 +8,38 @@ const NavbarMain = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [registro, setRegistro] = useState(false);
+
+  const handleCerrar = () => setShow(false);
+  const handleMostrar = () => setShow(true);
   return (
     <>
-      <Navbar bg="light" expand="lg" className="d-flex">
-        <Container>
-          <Link to="/">
-            <img
-              src="./images/logo.png"
-              alt="logo"
-              height="80"
-              className="pt-3 ms-2"
-            />
-          </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link className="text-dark" href="#home">
-                <b>Home</b>
-              </Nav.Link>
-              <Nav.Link className="text-dark" href="#features">
-                <b>Destacado</b>
-              </Nav.Link>
-              <Nav.Link className="text-dark" href="#pricing">
-                <b>Contacto</b>
-              </Nav.Link>
+      <Navbar bg="light" expand="lg" className="d-flex justify-content-around">
+        <Link to="/">
+          <img
+            src="./images/logo.png"
+            alt="logo"
+            height="80"
+            className="pt-3 ms-2"
+          />
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="">
+            <Nav.Link className="text-dark" href="#home">
+              <b>Home</b>
+            </Nav.Link>
+            <Nav.Link className="text-dark" href="#features">
+              <b>Destacado</b>
+            </Nav.Link>
+            <Nav.Link className="text-dark" href="#pricing">
+              <b>Contacto</b>
+            </Nav.Link>
+            <div className="d-flex justify-content-start">
               <Form className="d-flex">
                 <Form.Control
                   type="search"
-                  placeholder="Buscar noticias"
+                  placeholder="Buscar..."
                   className="me-2"
                   aria-label="Search"
                 />
@@ -50,7 +54,7 @@ const NavbarMain = () => {
               </Form>
               <Button
                 variant="outline-secondary"
-                className="d-flex align-items-center mx-0"
+                className="d-flex align-items-center mx-1"
                 size="sm"
               >
                 <div className="icono-fav">
@@ -62,7 +66,7 @@ const NavbarMain = () => {
               </Button>
               <Button
                 variant="danger"
-                className="d-flex align-items-center mx-0"
+                className="d-flex align-items-center mx-1"
                 onClick={handleShow}
               >
                 <div className="icono-login">
@@ -72,9 +76,18 @@ const NavbarMain = () => {
                   <b>Login</b>
                 </p>
               </Button>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+              <Button
+                variant="primary"
+                className="mx-1"
+                onClick={handleMostrar}
+              >
+                <p className="m-1">
+                  <b>Registrarse</b>
+                </p>
+              </Button>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
 
       <Modal show={show} onHide={handleClose}>
@@ -85,6 +98,54 @@ const NavbarMain = () => {
         </Modal.Header>
         <Container>
           <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Ingresa tu email</Form.Label>
+              <Form.Control type="email" placeholder="rollingFan@gmail.com" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Nunca compartas tu contraseña"
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+          </Form>
+        </Container>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* MODAL REGISTRO */}
+
+      <Modal show={registro} onHide={handleCerrar}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <b>Únete a Nosotros!</b>
+          </Modal.Title>
+        </Modal.Header>
+        <Container>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Ingresa tu Nombre y Apellido</Form.Label>
+              <Form.Control type="email" placeholder="Roberto Sanchez" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Ingresa tu email</Form.Label>
               <Form.Control type="email" placeholder="rollingFan@gmail.com" />
