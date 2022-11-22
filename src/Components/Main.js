c7d981c61d53055b0418b5fb1b96633a45f381a5
carolina-jpg <tejerinatemkincarolina@gmail.com>
2022-11-22 15:03:50 -0300

    detalles pagina admin y slider

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,6 +3,8 @@ import Footer from "./Footer";
 import Header from "./Header";
 import NavbarMain from "./NavbarMain";
 import Error404 from "./Error404";
+import PaginaAdminFeatured from "./PaginaAdminFeatured";
+import PaginaAdminUser from "./PaginaAdminUser";
 
 const Main = () => {
   return (
@@ -11,6 +13,8 @@ const Main = () => {
       <Header />
       <Footer />
       <Error404 />
+      <PaginaAdminFeatured />
+      <PaginaAdminUser />
     </>
   );
 };
