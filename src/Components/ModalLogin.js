411cea283ec4a79b12cdca6ef4140d55f1cef6e1
unknown <busimaximiliano@gmail.com>
2022-11-23 18:50:34 -0300

    validando login

--- src/Components/ModalLogin.js
+++ src/Components/ModalLogin.js
@@ -1,19 +1,38 @@
-import { Formik } from "formik";
+import { Field, Formik } from "formik";
 import React from "react";
 import { Container, Form } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
 const ModalLogin = ({ show, handleClose }) => {
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>
+        <Modal.Title className="text-center">
           <b>Bienvenido!</b>
         </Modal.Title>
       </Modal.Header>
       <Container>
-        <Formik initialValues={{ email: "", pass: "" }}>
+        <Formik
+          initialValues={{ email: "", pass: "" }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = "Éste campo esta vacio";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Email incorrecto";
+            }
+
+            if (!values.email) {
+              errors.pass = "Éste campo esta vacio";
+            }
+            return errors;
+          }}
+          onSubmit={(values) => {
+            console.log("values", values);
+          }}
+        >
           {({
             values,
             errors,
@@ -23,35 +42,39 @@ const ModalLogin = ({ show, handleClose }) => {
             handleSubmit,
             isSubmitting,
           }) => (
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail1">
-                <Form.Label name="email" values={values.email}>
-                  Ingresa tu email
-                </Form.Label>
-                <Form.Control type="email" placeholder="rollingFan@gmail.com" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label name="pass" values={values.pass}>
-                  Contraseña
-                </Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Nunca compartas tu contraseña"
+            <Container className="d-flex">
+              <Form className="w-100">
+                <label className="my-2">
+                  <b>Email</b>
+                </label>
+                <Field
+                  name="email"
+                  value={values.email}
+                  className={`form-control ${
+                    errors.email && touched.email && "is-invalid"
+                  }`}
                 />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-              <Modal.Footer>
-                <Button type="submit" variant="primary" onClick={handleClose}>
-                  Guardar cambios
-                </Button>
-              </Modal.Footer>
-            </Form>
+                {errors.email && touched.email && errors.email}
+                <br />
+                <label className="my-2">
+                  <b> Contraseña </b>
+                </label>
+                <Field
+                  name="pass"
+                  value={values.pass}
+                  className={`form-control ${
+                    errors.pass && touched.pass && "is-invalid"
+                  }`}
+                />
+                {errors.pass && touched.pass && errors.pass}
+                <br />
+                <input
+                  type={"submit"}
+                  value="Ingresar"
+                  className={"btn btn-success w-50 my-2"}
+                />
+              </Form>
+            </Container>
           )}
         </Formik>
       </Container>
