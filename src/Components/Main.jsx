73696e7b9e3146a6b3c903fae044b7d3d6c92a8e
CintiaUtrera <cintiaa_utrera@outlook.es>
2022-12-01 22:03:01 -0300

    mejorando rutas en main

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,26 +4,14 @@ import ArticleList from "./ArticleList";
 import Footer from "./Footer";
 import Header from "./Header";
 import NavbarMain from "./NavbarMain";
-import { Route, Routes } from "react-router-dom";
-import PaginaContacto from "./PaginaContacto";
-
 const Main = () => {
   return (
     <Container fluid className="d-flex flex-column min-vh-100 px-0">
       <NavbarMain />
       <Header />
-
-      <Footer />
-
-      <Routes>
-        
-        <Route path="/paginacontacto" element={<PaginaContacto />} />
-      </Routes>
-
       <ArticleList />
-      
+      <Footer />
     </Container>
   );
 };
-
 export default Main;
