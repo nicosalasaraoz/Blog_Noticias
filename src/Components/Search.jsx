c2bb0bc1b2a75cc28fe715f907ce7e33516f1bf9
CintiaUtrera <cintiaa_utrera@outlook.es>
2023-01-02 20:30:22 -0300

    agregando cards al search

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,25 +1,50 @@
 import React,{useState, useEffect} from 'react'
+import { Button, Card, Container, Row } from 'react-bootstrap'
+
 
 const Search  = () => {
 const [articles, setArticles] = useState([])
 const [search, setSearch] = useState('')
 
-
+// Cambiar URL por na de Noticias desde base de datos
 const URL = 'https://fakestoreapi.com/products'
 
 const showData = async () => {
     const response = await fetch(URL)
     const data = await response.json()
-    console.log(data)
+    
+    setArticles(data)
 }
 
-showData()
+const searcher = (e) =>
+    setSearch(e.target.value)
 
+useEffect( () => {
+    showData()
+}, [])
 
 
 
 return (
-    <div>Search</div>
+    <>
+        <Container>
+        <Row xs={1} md={2} className="g-4">
+            {articles.map((article)=>
+            <Card key={article.id} style={{ width: '10rem' }}>
+                <Card.Img variant="top" src={article.image} />
+                <Card.Body>
+                <Card.Title>{article.title}</Card.Title>
+                <Card.Text>
+                    {article.description}
+                </Card.Text>
+                <Button variant="primary">leer más..</Button>
+                <Card.Body/>
+            </Card.Body>
+            </Card>
+            )}
+        </Row>
+        </Container>
+    </>
 )
 }
 
